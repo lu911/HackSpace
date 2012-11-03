2d8003ca03412c8f1248c783780ad598cd6e4c2a
Cha SeongHo <be_attainer@naver.com>
2012-11-03 08:40:33 -0700

    * Add AuthView in challenge view

--- challenge/views.py
+++ challenge/views.py
@@ -3,7 +3,7 @@ from models import *
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-
+import json
 #@login_required(login_url='/login/')
 def ProbListView(request):
     try:   
@@ -14,7 +14,22 @@ def ProbListView(request):
         if tagID is not None:
             probData = ProbTag.get_from_prob(tagID)
         else:
-            probData = ProbTag.get_from_all_prob()
-    except:
+            raise ValueError
+    except ValueError:
         probData=ProbTag.get_from_all_prob()
     return render(request,'challenge/list.html',dict(tag_list=tagList, prob_data=probData))
+
+def AuthView(request):
+    auth=request.POST.get('auth')
+#    prob_id=request.POST.get('prob_id')
+    try:
+        prob=Problem.objects.get(prob_auth=auth)
+        if prob.prob_flag == 1:
+            result='already cleared'
+        else:
+            result='OK'
+            prob.prob_flag=1
+            prob.save()
+    except:
+        result='fail'
+    return HttpResponse(json.dumps(dict(auth=result)))
