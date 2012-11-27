08dc66f4a2fe84ab9ff9ee41ca72dd944942ec18
Cha SeongHo <be_attainer@naver.com>
2012-11-27 22:28:10 +0900

    * modified user_manager templates

--- admin/templatetags/admin_filter.py
+++ admin/templatetags/admin_filter.py
@@ -1,8 +1,10 @@
 from django import template
-
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 def getObject(value, arg):
     return value[arg.tag]
-
+def highlight(text, search):
+    return mark_safe(text.replace(search, "<span class='highlight'>%s</span>" % search))
 register.filter('getObject', getObject)
+register.filter('highlight', highlight)
