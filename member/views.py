0f0d2ce26bf871c358a96ac0f25140eb920f99a1
wiseple <wiseple@naver.com>
2012-11-29 02:08:46 +0900

    * Add login status check and Modify Template

--- member/views.py
+++ member/views.py
@@ -1,17 +1,19 @@
 #-*-coding:utf-8-*-
 from member.forms import *
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import login, logout
 from datetime import datetime
 
 def memberRegisterView(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             register_user(form)
-            return HttpResponseRedirect('/login/')
+            return HttpResponseRedirect('/')
     else:
         form = RegisterForm(initial=request.GET)
     return render(request, 'registration/register.html', dict(form=form))
@@ -27,13 +29,14 @@ def register_user(form):
     UserProfile.objects.create(user=user,score=0, last_solve_time=datetime.now())
 
 def memberLoginView(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
     if request.method == 'POST':
         form = AuthenticationForm(request.POST)
         if form.is_valid():
             user = form.get_user()
-            print user
             login(request, user)
-            return HttpResponseRedirect(request.POST.get('next', '/login/'))
+            return HttpResponseRedirect('/')
         else:
             return HttpResponseRedirect(request.GET.get('next', '/login/'))
     else:
